b5df0aba2bb85d07ac426584a288b2bb32e0c4ca
Lean-b <leandroborqueza@gmail.com>
2023-08-08 18:57:57 -0300

    wallet

--- lib.rs
+++ lib.rs
@@ -2,23 +2,15 @@
 
 #[ink::contract]
 mod wallet {
-    use ink::{primitives::AccountId, env::{call, caller}};
+    use ink::storage::Mapping;
 
-
-
+ 
     #[ink(storage)]
     pub struct Wallet {
         pub owner: AccountId,
         pub balances: Mapping<AccountId, Balance>,
     }
 
-    #[ink(event)]
-    pub struct Transfer {
-       from: AccountId,
-       to:AccountId,
-       value: Balance ,
-    }
-    
 
     #[ink(event)]
     pub struct Withdraw {
@@ -29,34 +21,28 @@ mod wallet {
     #[ink(event)]
     pub struct Deposit {
         from: AccountId,
-        value: balance,
+        value: Balance,
     }
 
-     
-
+    #[derive(Debug,PartialEq, Eq, scale::Encode , scale::Decode)]  
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
         InsufficientBalance,
     }
 
-    pub type Result<T> = core::result::Result<T,Error>;
+    pub type Result<T> = core::result::Result<T, Error>;
 
     impl Wallet {
         #[ink(constructor)]
         pub fn new(owner: AccountId) -> Self {
+            let balances = Mapping::default();
             Self {
                 owner,
-                balances: 0,
+                balances,
             }
         }
 
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self {
-                owner: Default::default(),              
-            }
-        }
-
+ 
         #[ink(message, payable)]
         pub fn deposit(&mut self) -> Result<()> {
             let value: Balance  = self.env().transferred_value();
@@ -65,7 +51,15 @@ mod wallet {
                 return Err(Error::InsufficientBalance)
             }
             
-            self.balance += value;
+            let caller = self.env().caller();
+            let balances = self.balances.get(&caller).unwrap();
+
+            self.balances.insert(caller, &(balances + value));
+
+            self.env().emit_event( Deposit {
+                from: caller,
+                value,
+            });
            
             Ok(())
         }
@@ -89,98 +83,18 @@ mod wallet {
 
             Ok(())
         }
-
-        
-
-        #[ink(message)]
-        pub fn transfer(&mut self, from: AccountId, to: AccountId, amount: Balance) -> Result<(),Error> {
-            
-                
-            Ok(())
-        }
+       
        
        
         #[ink(message)]
-        pub fn balance(&self) -> Balance {
-            self.balances
+        pub fn balance(&self) -> Option<Balance> {
+            let caller = self.env().caller();
+            self.balances.get(caller)
         }
         
         #[ink(message)]
-        pub fn get_owner(&self, owner: AccountId) -> AccountId {
+        pub fn get_owner(&self) -> AccountId {
             self.owner
         }
     }
 }
-
-/// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-///
-/// When running these you need to make sure that you:
-/// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-/// - Are running a Substrate node which contains `pallet-contracts` in the background
-#[cfg(all(test, feature = "e2e-tests"))]
-mod e2e_tests {
-    /// A helper function used for calling contract messages.
-    use ink_e2e::build_message;
-
-    /// Imports all the definitions from the outer scope so we can use them here.
-    use super::*;
-
-    /// The End-to-End test `Result` type.
-    type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-    /// We test that we can upload and instantiate the contract using its default constructor.
-    #[ink_e2e::test]
-    async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-        // Given
-        let constructor = WalletRef::default();
-
-        // When
-        let contract_account_id = client
-            .instantiate("wallet", &ink_e2e::alice(), constructor, 0, None)
-            .await
-            .expect("instantiate failed")
-            .account_id;
-
-        // Then
-        let get = build_message::<WalletRef>(contract_account_id.clone())
-            .call(|wallet| wallet.get());
-        let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-        assert!(matches!(get_result.return_value(), false));
-
-        Ok(())
-    }
-
-    /// We test that we can read and write a value from the on-chain contract contract.
-    #[ink_e2e::test]
-    async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-        // Given
-        let constructor = WalletRef::new(false);
-        let contract_account_id = client
-            .instantiate("wallet", &ink_e2e::bob(), constructor, 0, None)
-            .await
-            .expect("instantiate failed")
-            .account_id;
-
-        let get = build_message::<WalletRef>(contract_account_id.clone())
-            .call(|wallet| wallet.get());
-        let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-        assert!(matches!(get_result.return_value(), false));
-
-        // When
-        let flip = build_message::<WalletRef>(contract_account_id.clone())
-            .call(|wallet| wallet.flip());
-        let _flip_result = client
-            .call(&ink_e2e::bob(), flip, 0, None)
-            .await
-            .expect("flip failed");
-
-        // Then
-        let get = build_message::<WalletRef>(contract_account_id.clone())
-            .call(|wallet| wallet.get());
-        let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-        assert!(matches!(get_result.return_value(), true));
-
-        Ok(())
-    }
-}
-
